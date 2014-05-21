da9ec9316960e4712a51a060a96339512dafee8f
oefish <oefish@oefishrunner>
2014-05-20 23:03:16 -0500

    Initial

--- src/com/boldust/math001/Math001.java
+++ src/com/boldust/math001/Math001.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Copied from NightCracker C version but actually not for BigInteger.
  */
 package com.boldust.math001;
 
