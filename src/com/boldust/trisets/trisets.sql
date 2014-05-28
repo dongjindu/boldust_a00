fa082a2644ab0ea263f9f4ae3c6b6c1537973979
oefish <oefish@oefishrunner>
2014-05-27 22:50:31 -0500

    Initial

--- src/com/boldust/trisets/trisets.sql
+++ src/com/boldust/trisets/trisets.sql
@@ -3,23 +3,26 @@ create table tables(tablename varchar(20), tabletype int, comment varchar(30));
 create table tablecols(tablename varchar(20), colname varchar(20), fieldname varchar(25), 
                                                                    fieldtype int, primary key (tablename, colname));
 --Field
-create table fields(fieldname varchar(25), fieldtype int, fieldlength int, fielddecimal int, 
+create table fields(fieldname varchar(25), version varchar(20), isactive boolean, fieldtype int, fieldlength int, fielddecimal int, 
                                           displaylength int, displaydecimal int, fieldimp varchar(25)
    , primary key (fieldname));  
 create table fieldtypes(fieldtype int, 
+                        version varchar(20),
+                        isactive boolean,
                         islengthvar boolean,
                         isdecimalvar boolean,
                         fieldtypeimp varchar(25), 
                         priamry key (fieldtype));
-insert into fieldtypes values ('1', false, false, 'Integer'),
-                              ('2', false, false, 'Bollean'),
-                              ('3', true, false,  'String'),
-                              ('4', false, false, 'Bigint'),
-                              ('5', false, false, 'Float'),
-                              ('6', false, false, 'Double'),
-                              ('7', false, false, 'BigDecimal'),
-                              ('8', false, false, 'Stream');
-                              ('9', true, true, 'Pack');
+insert into fieldtypes 
+                       values ('1', '1.0', true, false, false, 'Integer'),
+                              ('2', '1.0', true, false, false, 'Bollean'),
+                              ('3', '1.0', true, true, false,  'String'),
+                              ('4', '1.0', true, false, false, 'Bigint'),
+                              ('5', '1.0', true, false, false, 'Float'),
+                              ('6', '1.0', true, false, false, 'Double'),
+                              ('7', '1.0', true, false, false, 'BigDecimal'),
+                              ('8', '1.0', true, false, false, 'Stream');
+                              ('9', '1.0', true, true, true, 'Pack');
 /* 
     public static final int TYPE_INT = 1;
     public static final int TYPE_BOOL = 2;
